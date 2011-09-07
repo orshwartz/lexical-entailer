86a63e8e6ff7e4cad9c7bdd0371ebfb2a8bb8809
shwarto <shwarto@d182743f-9115-efb0-7ddf-1fab01ec802d>
2011-09-07 05:37:23 +0000

    First partial code.

---
+++ trunk/src/entailer/ProcessedSet.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package entailer;
+
+import java.util.ArrayList;
+
+/**
+ * @author Or Shwartz
+ */
+public class ProcessedSet <PhraseType extends ProcessedCandidatePhrase> {
+
+	private ArrayList<PhraseType> topics;
+}
