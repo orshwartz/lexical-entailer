461522aef5acd0cd510e336d9c1cde78f520a65d
shwarto <shwarto@d182743f-9115-efb0-7ddf-1fab01ec802d>
2011-10-29 23:35:13 +0000

    Main class, for running the project.

---
+++ src/entailer/Main.java
@@ -0,0 +1,15 @@
+package entailer;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		ProcessedDevSetParser devSetParser = new ProcessedDevSetParser("res/DevSet/Processed_DevSet.txt");
+		
+
+	}
+
+}
