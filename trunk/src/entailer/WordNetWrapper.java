280278d69a73329f669f948d142c34d3282b6f9d
shwarto <shwarto@d182743f-9115-efb0-7ddf-1fab01ec802d>
2011-11-15 00:39:46 +0000

    First version of WordNetWrapper - for finding expansion words for given lemma.

--- trunk/src/entailer/WordNetWrapper.java
+++ trunk/src/entailer/WordNetWrapper.java
@@ -2,18 +2,25 @@ package entailer;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import net.sf.extjwnl.JWNLException;
 import net.sf.extjwnl.data.IndexWord;
-import net.sf.extjwnl.data.IndexWordSet;
 import net.sf.extjwnl.data.POS;
+import net.sf.extjwnl.data.PointerUtils;
+import net.sf.extjwnl.data.Synset;
+import net.sf.extjwnl.data.Word;
+import net.sf.extjwnl.data.list.PointerTargetTreeNode;
+import net.sf.extjwnl.data.list.PointerTargetTreeNodeList.Operation;
 import net.sf.extjwnl.dictionary.Dictionary;
 
 public class WordNetWrapper {
 
 	private final static String PATH_FILE_PROPERTIES =
 		"res/extjwnl/file_properties.xml";
+	
+	private final static int HYPERNYM_DEPTH = 2;
 	
 	private Dictionary dictionary;
 	
@@ -40,9 +47,62 @@ public class WordNetWrapper {
 	 * @param word
 	 * @return
 	 */
-	public ArrayList<IndexWord> getSimilarWords(POS pos, String word) {
+	public HashMap<String,Word> getSimilarWords(POS pos, String lemma) {
 		
-		ArrayList<IndexWord> returnedWords = new ArrayList<IndexWord>();
+		HashMap<String,Word> returnedWords = new HashMap<String,Word>();
+		
+		try {
+			IndexWord indexWordForGivenLemma =
+				dictionary.lookupIndexWord(pos, lemma);
+			
+			// If the word was found
+			if (indexWordForGivenLemma != null) {
+				
+				// Add word synonyms and hypernyms to returned results
+				List<Synset> senses = indexWordForGivenLemma.getSenses();
+				for (Synset synset : senses) {
+					
+					// Add synonyms to returned results
+					List<Word> synsetWords = synset.getWords();
+					for (Word curWord : synsetWords) {
+						returnedWords.put(curWord.getLemma(), curWord);
+					}
+					
+//					// Add hypernyms to returned results
+//					List<Pointer> synsetHypernyms =
+//						synset.getPointers(PointerType.HYPERNYM);
+//					for (Pointer curHyponym : synsetHypernyms) {
+//						
+//						curHyponym.getTargetSynset().getWords();
+//						
+//						synsetWords = synset.getWords();
+//						for (Word curWord : synsetWords) {
+//							returnedWords.put(curWord.getLemma(), curWord);
+//						}
+//					}
+					
+					// Add all hypernyms on tree paths for specified depth
+					Operation opr = new Operation() {
+						@Override
+						public PointerTargetTreeNode execute(PointerTargetTreeNode node) {
+							return node;
+						}
+					};
+					List<PointerTargetTreeNode> hypernyms =
+						PointerUtils.getHypernymTree(synset, HYPERNYM_DEPTH).getAllMatches(opr);
+					for (PointerTargetTreeNode curHypernym : hypernyms) {
+						List<Word> hypernymSynset =
+							curHypernym.getSynset().getWords();
+						for (Word curHypernymWord : hypernymSynset) {
+							returnedWords.put(curHypernymWord.getLemma(), curHypernymWord);
+						}
+					}
+				}
+			}
+		} catch (JWNLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return returnedWords;
 	}
