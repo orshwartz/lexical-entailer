788921296bc1d25a2118237420697a1abb0a624e
shwarto <shwarto@d182743f-9115-efb0-7ddf-1fab01ec802d>
2011-11-15 00:47:43 +0000

    First version of WordNetWrapper - for finding expansion words for given lemma. These files were altered during this change, for tests.

--- src/entailer/Main.java
+++ src/entailer/Main.java
@@ -1,9 +1,11 @@
 package entailer;
 
 import java.io.FileInputStream;
-import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 import net.sf.extjwnl.data.POS;
+import net.sf.extjwnl.data.Word;
 import net.sf.extjwnl.dictionary.Dictionary;
 
 public class Main {
@@ -20,8 +22,17 @@ public class Main {
 
 		ProcessedDevSetParser devSetParser =
 			new ProcessedDevSetParser("res/DevSet/Processed_DevSet.txt");
-		
+Date date = new Date(); long start = date.getTime();		
 		Dictionary dictionary =
 			Dictionary.getInstance(new FileInputStream(PATH_FILE_PROPERTIES));
+
+		WordNetWrapper wordNetWrapper = new WordNetWrapper();
+		HashMap<String,Word> similarWords =
+			wordNetWrapper.getSimilarWords(POS.NOUN, "golden retriever");
+		for (String word : similarWords.keySet()) {
+			System.out.println(word);
+		}
+Date date2 = new Date(); long end = date2.getTime();
+System.out.println(end-start);
 	}
 }
